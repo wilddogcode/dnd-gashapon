3cbd07e6287c42ce24db16dbc7bbb75fa6c36474
Legend Vildhund <legendwolfbusiness@gmail.com>
2020-05-13 17:25:31 -0400

    Final of v1

--- capsule.js
+++ capsule.js
@@ -1,3 +1,5 @@
+var previous;
+
 const items = [
     ["Backpack", "https://s3.amazonaws.com/gashapon.alby/backpack.jpg"],
     ["Body", "https://s3.amazonaws.com/gashapon.alby/body.jpg"],
@@ -13,18 +15,13 @@ function getItem() {
     var index = Math.round(Math.random() * 7);
     var gName = items[index][0];
     var gUrl = items[index][1];
-    newItem(gName, gUrl);
-    console.log(`The name is ${gName}`);
-    // var exist = !!document.getElementsByClassName('itemimg');
-    // exist ? newItem(gName, gUrl) : replaceItem(gName, gUrl);
+    index == previous ? getItem() : newItem(gName, gUrl, index);
+    return;
 }
 
-function newItem(name, url) {
+function newItem(name, url, ind) {
     document.getElementById('itemimg').src = url;
     document.getElementById('itemname').innerHTML = name;
+    previous = ind;
+    return;
 }
-
-// function replaceItem(name, url) {
-//     document.getElementsByClassName('itemimg').src = url;
-//     document.getElementsByClassName('itemname').innerHTML = name;
-// }
